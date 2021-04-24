a328a08e1ef503d99e64f52f52b45445c1c9a026
Kamila Tarnożek <tomaszposiadala@gmail.com>
2021-04-24 17:20:50 +0100

    finished styling price card component

--- src/components/PriceCard.js
+++ src/components/PriceCard.js
@@ -4,13 +4,13 @@ import ButtonRectangle from './ButtonRectangle'
 
 // Price Card Component, pass 'secondary' prop for PRO plan design
 
-const PriceCard = ({ secondary, name, text, priceM, priceY, paymentOption, ...otherProps }) => (
+const PriceCard = ({ secondary, name, text, priceM, priceY, paymentOption }) => (
     <StyledPriceCard secondary={secondary}>
         {secondary && <GradientLine />}
         <h2 className=".smaller-h2">{name}</h2>
-        <p>{text}</p>
+        <p className="content-card">{text}</p>
         <p className="price">$ {paymentOption ? priceM : priceY}</p>
-        <p>per {paymentOption ? 'month' : 'year'}</p>
+        <p className="pay-option">per {paymentOption ? 'month' : 'year'}</p>
         {secondary ? (
             <ButtonRectangle secondary>PICK PLAN</ButtonRectangle>
         ) : (
@@ -26,6 +26,33 @@ const StyledPriceCard = styled.div`
     height: 41rem;
     background-color: ${props => (props.secondary ? `var(--black)` : `var(--primaryCardBackground)`)};
     color: ${props => (props.secondary ? `var(--white)` : `var(--black)`)};
+    margin-bottom: 2.5rem;
+    padding: 5rem 4rem;
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    h2 {
+        margin-bottom: 2rem;
+        letter-spacing: 0;
+    }
+    .content-card {
+        text-align: center;
+        margin-bottom: 4rem;
+    }
+    .price {
+        font-weight: 700;
+        font-size: 4rem;
+        letter-spacing: 4px;
+        line-height: 4.8rem;
+        margin-bottom: 0.5rem;
+        color: inherit;
+    }
+    .pay-option {
+        margin-bottom: 4rem;
+    }
 `
 const GradientLine = styled.div`
     width: 100%;
